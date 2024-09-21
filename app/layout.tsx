e93a9c916f8936847a230f411d48a1b33e9b57f6
Aron Vischjager <aron@vischjager.com>
2024-09-21 19:19:32 +0200

    Update to code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
-import { Layout } from "@/features/layout";
+import { Layout } from "@/components/layout";
 import { muiTheme } from "@/features/theme";
 import "@/styles/globals.css";
 
