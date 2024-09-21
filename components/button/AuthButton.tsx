42fd7ae86ace6c447977d8ab81eef173c7ef0da8
Aron Vischjager <aron@vischjager.com>
2024-09-21 19:08:39 +0200

    Update to auth button logic

--- components/button/AuthButton.tsx
+++ components/button/AuthButton.tsx
@@ -5,7 +5,7 @@ import Button from "@mui/material/Button";
 
 export function AuthButton() {
   // Hooks and global state
-  const [title, setTitle] = useState<string>("");
+  const [title, setTitle] = useState<string>("Connect");
 
-  return <Button variant="contained"></Button>;
+  return <Button variant="contained">{title}</Button>;
 }
--- features/layout/components/Navbar.tsx
+++ features/layout/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
+import { AuthButton } from "@/components/button/AuthButton";
 import styles from "./Navbar.module.css";
 
 export function Navbar() {
   return (
     <div id={styles.navbar}>
       <div>Token Transfer</div>
-      <Button variant="contained">Connect</Button>
+      <AuthButton></AuthButton>
     </div>
   );
 }
