c3b632aefb7b605b2a35cfd4e404ac0cab348bc1
Aron Vischjager <aron@vischjager.com>
2024-09-23 20:04:44 +0200

    Update to number function

--- features/transfer/index.tsx
+++ features/transfer/index.tsx
@@ -1,8 +1,8 @@
 'use client'
 
+import { useGetWalletBalance } from '@/hooks'
 import { Button, TextField } from '@mui/material'
 import styles from './transfer.module.css'
-import { useGetWalletBalance } from '@/hooks'
 
 export function TransferScreen() {
     // Hooks and global state
@@ -22,9 +22,7 @@ export function TransferScreen() {
             </Button>
             <div id={styles.info}>
                 <h2>Transfer Info</h2>
-                <div className={styles.infoText}>
-                    Balance: {walletBalance.toString()}
-                </div>
+                <div className={styles.infoText}>Balance: {walletBalance}</div>
                 <div className={styles.infoText}>Status {}</div>
                 <div className={styles.infoText}>Hash: {}</div>
             </div>
